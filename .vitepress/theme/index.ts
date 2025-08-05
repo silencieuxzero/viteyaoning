31dbaee1244b509eb2ae79ce2b13b98dafab4b22
“silencieux” <rain_wind1217@outlook.com>
2025-08-05 21:36:25 +0800

    一般修改

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,10 +5,13 @@ import "./style.css";
 import './style/index.css'
 import MouseClick from "./components/MouseClick.vue"
 import MouseFollower from "./components/MouseFollower.vue"
+import { NProgress } from 'nprogress-v2/dist/index.js' // 进度条组件
+import 'nprogress-v2/dist/index.css' // 进度条样式
 import Linkcard from "./components/Linkcard.vue"
 import HomeUnderline from "./components/HomeUnderline.vue"
 // import "element-plus/dist/index.css";
 import GLayout from "./GLayout.vue";
+import { inBrowser } from 'vitepress'
 
 export default {
   extends: DefaultTheme,
@@ -21,5 +24,15 @@ export default {
     app.component('MouseFollower' , MouseFollower)
     app.component('Linkcard' , Linkcard)
     // ...
+
+    if (inBrowser) {
+      NProgress.configure({ showSpinner: false })
+      router.onBeforeRouteChange = () => {
+        NProgress.start() // 开始进度条
+      }
+      router.onAfterRouteChanged = () => {
+         NProgress.done() // 停止进度条
+      }
+    }
   },
 } satisfies Theme;
